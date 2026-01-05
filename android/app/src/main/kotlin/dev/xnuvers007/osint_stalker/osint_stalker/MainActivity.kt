71af30e56113826c5656fbcaafab492b74974727
Xnuvers007 <indra111.ida@gmail.com>
2026-01-06 03:25:18 +0700

    fix: update MainActivity to extend FlutterFragmentActivity (Biometric error)

--- android/app/src/main/kotlin/dev/xnuvers007/osint_stalker/osint_stalker/MainActivity.kt
+++ android/app/src/main/kotlin/dev/xnuvers007/osint_stalker/osint_stalker/MainActivity.kt
@@ -1,5 +1,5 @@
 package dev.xnuvers007.osint_stalker.osint_stalker
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
